c5dad93243a5940a1a7273b3c8ef13e37d1dd069
txich <txichh@gmail.com>
2025-06-09 12:13:41 +0200

    added security, implemented call method

--- contracts/sharedwallet.sol
+++ contracts/sharedwallet.sol
@@ -1,40 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-contract SharedWallet {
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract SharedWallet is ReentrancyGuard {
 
     event Deposit(address indexed sender, uint amount, uint balanceafter, uint timestamp );
     event Withdrawal(address indexed sender, uint amount, uint balanceafter, uint timestamp);
 
     mapping(address => uint) public balances;
 
-    receive() external payable {
-        balances[msg.sender] += msg.value;
-        emit Deposit(msg.sender, msg.value, balances[msg.sender], block.timestamp);
-    }
-
     function myBalance() view external returns (uint) {
         return balances[msg.sender]; 
     }
 
-    function deposit() external payable {
+    function deposit() public payable {
         balances[msg.sender] += msg.value;
         emit Deposit(msg.sender, msg.value, balances[msg.sender], block.timestamp);
     }
-    
-    function withdraw(uint amount) external  {
+
+    receive() external payable {
+        deposit();
+    }
+
+    function withdraw(uint amount) external nonReentrant {
         require (amount <= balances[msg.sender], "Insufficient balance");
+
         balances[msg.sender] -= amount;
-        payable(msg.sender).transfer(amount);
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        require(success, "Transfer failed");
+
         emit Withdrawal(msg.sender, amount, balances[msg.sender], block.timestamp);
     }
 
-    function withdrawAll() external {
+    function withdrawAll() external nonReentrant {
         require(balances[msg.sender] > 0, "Insufficient balance");
+
         uint amount = balances[msg.sender];
         balances[msg.sender] = 0;
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        require(success, "Transfer failed");
+
         emit Withdrawal(msg.sender, amount, 0, block.timestamp);
-        payable(msg.sender).transfer(amount);
 
     }
 }
